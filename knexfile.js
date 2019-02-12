55a30448e89b85de0264581861f3a48222e1a290
Karin Melin <karin.melin@valtech.com>
2019-02-12 11:24:07 +0100

    pg -> postgresql

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: `${process.env.DATABASE_URL}${process.env.NODE_ENV === 'production' ? '?ssl=require' : ''}`,
     pool: {
       min: 2,
